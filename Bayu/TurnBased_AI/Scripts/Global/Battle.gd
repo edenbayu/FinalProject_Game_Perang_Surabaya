64c7ab64bd4bad941a09add5d95aa1005bc8353b
unknown <bayueden4@gmail.com>
2024-08-11 08:47:00 +0700

    added displayed damage v3.12

--- Bayu/TurnBased_AI/Scripts/Global/Battle.gd
+++ Bayu/TurnBased_AI/Scripts/Global/Battle.gd
@@ -46,9 +46,12 @@ func apply_damage():
 func attack_hp(active_unit: Unit, target: Unit):
 	var final_damage = clamp(apply_reduction(active_unit.damage, target.curr_armor), 0, active_unit.damage)
 	target.curr_health -= final_damage
+	display_number(final_damage, Vector2(target.global_position.x, target.global_position.y - 126), dmg_type)
 
 func attack_armor(active_unit: Unit, target: Unit):
-	target.curr_armor -= active_unit.damage
+	var final_damage = active_unit.damage
+	target.curr_armor -= final_damage
+	display_number(final_damage, Vector2(target.global_position.x, target.global_position.y - 126), dmg_type)
 
 func apply_reduction(base_damage: int, current_armor: int) -> int:
 	var final_damage = base_damage
@@ -64,3 +67,44 @@ func apply_reduction(base_damage: int, current_armor: int) -> int:
 		return final_damage
 	return final_damage
 ## End of Attack Code ##
+
+## DISPLAY ATTACK DAMAGE ##
+func display_number(value: int, position: Vector2, damage_type: String):
+	print("damage should be displayed here!")
+	var number = Label.new()
+	number.global_position = position
+	number.text = str(value)
+	number.z_index = 5
+	number.label_settings = LabelSettings.new()
+	
+	var color = "#FFF"
+	match damage_type:
+		"hp_damage":
+			color = "#E63B60"
+		"armor_damage":
+			color = "#4895EF"
+	
+	number.label_settings.font_color = color
+	number.label_settings.font_size = 40
+	number.label_settings.outline_color = "#000"
+	number.label_settings.outline_size = 12
+	
+	call_deferred("add_child", number)
+	
+	await number.resized
+	number.pivot_offset = Vector2(number.size / 2)
+	
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(
+		number, "position:y", number.position.y - 24, 0.25
+	).set_ease(Tween.EASE_OUT)
+	tween.tween_property(
+		number, "position:y", number.position.y, 0.5
+	).set_ease(Tween.EASE_IN).set_delay(0.25)
+	tween.tween_property(
+		number, "scale", Vector2.ZERO, 0.25
+	).set_ease(Tween.EASE_IN).set_delay(0.5)
+	
+	await tween.finished
+	number.queue_free()
